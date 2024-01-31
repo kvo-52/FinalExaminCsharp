9bd472913d785fe79c1c14c3e9e92c71aa565374
kvo52@kvo1 <kvo.52@mail.ru>
2024-01-31 12:09:34 +0300

    web_app_test

---
+++ TestProject1/RSAToolsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppUser.Controllers;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class RSAToolsTests
+    {
+        [Test]
+        public void TestDecryptMethod()
+        {
+            string path = ".\\WebApplUser\\rsa\\private_key.pem";
+            RSA plaintext = RSATools.GetPrivateKey(path);
+
+            Assert.NotNull(plaintext);
+        }
+
+    }
+}
+
--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0542992165577c4043ea21b02c7f027a67c9559")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
